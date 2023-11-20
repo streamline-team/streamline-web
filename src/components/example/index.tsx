dcfa0c798771a414ed5f61ac7a29ae72e2f858c4
Bruno Silva <bruno.silva@ada.ac.uk>
2023-11-20 15:17:43 +0000

    Upload core project files

---
+++ src/components/example/index.tsx
@@ -0,0 +1,11 @@
+import './example.css'
+
+const ExampleComponent = (): JSX.Element => {
+
+    return (
+        <div className="componentTest">I am an example component</div>
+    )
+
+}
+
+export default ExampleComponent
