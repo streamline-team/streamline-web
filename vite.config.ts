2085733a5446557f4bf10ef7b1744e93942550ee
Bruno Silva <54673205+brunogrcsada@users.noreply.github.com>
2023-11-22 11:31:49 +0000

    Basic auth setup with Kinde (#3)
    * Basic auth setup with kinde
    
    * Get kinde env vars from .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  server: {
+    port: 3030,
+  },
   plugins: [react()],
-})
+});
