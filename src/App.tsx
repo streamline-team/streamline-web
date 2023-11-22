24f9c85988a3bc883ba677d88ea79ae2df41d83b
Bruno Silva <bruno.silva@ada.ac.uk>
2023-11-22 11:17:33 +0000

    Basic auth setup with kinde

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import ExampleComponent from './components/example'
+import { useKindeAuth } from '@kinde-oss/kinde-auth-react'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const { login, isAuthenticated, isLoading, user } = useKindeAuth()
+
+  useEffect(() => {
+    if (isAuthenticated || isLoading) {
+      return
+    }
+
+    const loginRedirect = async (): Promise<void> => {
+      await login()
+    }
+
+    loginRedirect()
+  }, [isAuthenticated, isLoading])
+
+  if (!user) {
+    return (
+      <>
+        <span>Not Authenticated!</span>
+      </>
+    )
+  }
+
   return (
     <>
-    <ExampleComponent></ExampleComponent>
+
+      <ExampleComponent></ExampleComponent>
       <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {KindeProvider} from "@kinde-oss/kinde-auth-react";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <KindeProvider
+      clientId="e4f2eb91e927409193df3165d6616a46"
+      domain="https://streamline-staging.eu.kinde.com"
+      redirectUri="http://localhost:3030"
+      logoutUri="http://localhost:3030"
+    >
+      <App />
+    </KindeProvider>
   </React.StrictMode>,
 )
