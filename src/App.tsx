87e7c11058077435bd198d718d0710cb48efb9f9
Bruno Silva <bruno.silva@ada.ac.uk>
2023-11-23 09:56:24 +0000

    Deprecate eslint for now, task endpoint example

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,70 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import ExampleComponent from './components/example'
-import { useKindeAuth } from '@kinde-oss/kinde-auth-react'
+import { useEffect, useState } from "react";
+import "./App.css";
+import ExampleComponent from "./components/example";
+import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
+
+interface Task {
+  createdAt: string;
+  description: string;
+  done: boolean;
+  dueDate: string | null;
+  id: number;
+  priority: number;
+  title: string;
+  updatedAt: string;
+  userId: number;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const { login, isAuthenticated, isLoading, user } = useKindeAuth()
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const { login, isAuthenticated, isLoading, user } = useKindeAuth();
 
   useEffect(() => {
     if (isAuthenticated || isLoading) {
-      return
+      return;
     }
 
     const loginRedirect = async (): Promise<void> => {
-      await login()
-    }
+      await login();
+    };
 
-    loginRedirect()
-  }, [isAuthenticated, isLoading])
+    loginRedirect();
+  }, [isAuthenticated, isLoading]);
+
+  useEffect(() => {
+    const fetchData = async (): Promise<void> => {
+      const serviceUrl = import.meta.env.VITE_STREAMLINE_SERVICES_URL;
+
+      fetch(`${serviceUrl}/task`)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data) {
+            setTasks(data.data);
+          }
+        });
+    };
+
+    if (user) {
+      fetchData();
+    }
+  }, [user]);
 
   if (!user) {
     return (
       <>
         <span>Not Authenticated!</span>
       </>
-    )
+    );
   }
 
   return (
     <>
+      {tasks.map((task, id) => (
+        <div key={id}>{task.title}</div>
+      ))}
       <ExampleComponent></ExampleComponent>
-      <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import {KindeProvider} from "@kinde-oss/kinde-auth-react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <KindeProvider
-      clientId={import.meta.env.VITE_KINDE_CLIENT_ID}
-      domain={import.meta.env.VITE_KINDE_DOMAIN || ''}
-      redirectUri={import.meta.env.VITE_KINDE_REDIRECT_URI || ''}
-      logoutUri={import.meta.env.VITE_KINDE_LOGOUT_URI}
-    >
-      <App />
-    </KindeProvider>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <KindeProvider
+    clientId={import.meta.env.VITE_KINDE_CLIENT_ID}
+    domain={import.meta.env.VITE_KINDE_DOMAIN || ""}
+    redirectUri={import.meta.env.VITE_KINDE_REDIRECT_URI || ""}
+    logoutUri={import.meta.env.VITE_KINDE_LOGOUT_URI}
+  >
+    <App />
+  </KindeProvider>
+);
